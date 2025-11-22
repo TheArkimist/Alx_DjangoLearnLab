3750f6dd9ddab0bbd5557649cb6e4875c4b00aad
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-22 22:56:01 +0000

    API project configuring serializers, views and urls

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework import viewsets
 
-class BookList(generics.ListAPIView):
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer

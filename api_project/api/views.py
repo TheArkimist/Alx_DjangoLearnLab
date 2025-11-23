2ef89d1b82496b8d7365ada5ae4c66e1278a3c3e
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-23 14:10:38 +0000

    API project apply authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,12 +2,18 @@ from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]

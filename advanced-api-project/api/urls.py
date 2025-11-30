c840736546dd83622d96897185af7aec2448347a
Ci$c0 <emmanueloduro37@gmail.com>
2025-11-30 21:56:07 +0000

    CRUD fix

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import BookListView
+from .views import BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-   path('books/', BookListView.as_view(), name='book-list'),
+   path('books/list', BookListView.as_view(), name='book-list'),
+   path('books/create/', BookCreateView.as_view(), name='book-list'),
+   path('books/update/', BookUpdateView.as_view(), name='book-list'),
+   path('books/delete/', BookDeleteView.as_view(), name='book-list'),
 ]

7d987521156328d86da2188d5a963175d7b96212
Ci$c0 <emmanueloduro37@gmail.com>
2025-12-12 03:35:37 +0000

    Social media api initial commit fix

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,3 +7,6 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_images/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.username
--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,55 +1,18 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from .models import CustomUser
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture', 'followers', 'following']
-        read_only_fields = ['id', 'followers', 'following']
-
-
-
+from django.contrib.auth import get_user_model
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     password2 = serializers.CharField(write_only=True, required=True, label='Confirm password', style={'input_type': 'password'})
-
-
     class Meta:
-        model = CustomUser
+        model = get_user_model()  # Use get_user_model() to get the User model dynamically
         fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name', 'bio']
-
 
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError({"password": "Passwords must match."})
         return data
 
-
     def create(self, validated_data):
-        validated_data.pop('password2', None)
-        password = validated_data.pop('password')
-        user = CustomUser(**validated_data)
-        user.set_password(password)
-        user.save()
-        # create token for the user
-        Token.objects.create(user=user)
+        user = get_user_model().objects.create_user(**validated_data)
         return user
-
-
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, write_only=True, style={'input_type': 'password'})
-
-
-    def validate(self, data):
-        user = authenticate(username=data.get('username'), password=data.get('password'))
-        if not user:
-            raise serializers.ValidationError('Unable to log in with provided credentials.')
-        data['user'] = user
-        return data
